a411c0a292bed33dd9e964b1872b93dac5e4e083
yujou0 <abc83327@gmail.com>
2023-05-24 16:12:54 +0800

    加入專案檔案。

---
+++ ASPNETMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    //繼承DbContext類，以便擁有資料庫連接和操作資料庫的功能 : DbContext
+    //然後對著MVCDemoDbContext按右鍵 建立建構函式(options)
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        //打prop按兩下tab有快截鍵建立屬性
+        //先在其他地方建立好Employee類別
+        //Employees會是table name
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
